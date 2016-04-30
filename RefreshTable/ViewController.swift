e148dcfef09b9f94c8dbbbd4b81a3bcf82a64545
bhuman <cptdankoAir@bhumans-MacBook-Air.local>
2016-04-30 20:27:34 +1000

    initial commit

--- RefreshTable/ViewController.swift
+++ RefreshTable/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UITableView {
+    func refreshTable(){
+        let indexPathForSection = NSIndexSet(index: 0)
+        self.reloadSections(indexPathForSection, withRowAnimation: UITableViewRowAnimation.Middle)
+    }
+}
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var reloadBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    var data = ["One", "two", "three"]
+    var origData:[String]?
+    let indexPathForSection = NSIndexSet(index: 0)
+    var section = 1
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
+    @IBAction func reloadTable(sender: AnyObject) {
+        data = ["four","five","six"]
+        origData = data
+        tableView.refreshTable()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 }
 
